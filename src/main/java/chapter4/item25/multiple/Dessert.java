6d7e0ad6788be07281a81aeec127c980bfe875f7
Ethan <tinhlam141202@gmail.com>
2025-11-28 15:52:23 +0700

    Finish chapter 4 item 25

---
+++ src/main/java/chapter4/item25/multiple/Dessert.java
@@ -0,0 +1,10 @@
+package chapter4.item25.multiple;
+
+// Java compiler doesn't accept this.
+//class Utensil {
+//    static final String NAME = "pot";
+//}
+//
+//class Dessert {
+//    static final String NAME = "pie";
+//}
---
+++ src/main/java/chapter4/item25/multiple/Utensil.java
@@ -0,0 +1,9 @@
+package chapter4.item25.multiple;
+
+class Utensil {
+    static final String NAME = "pan";
+}
+
+class Dessert {
+    static final String NAME = "cake";
+}
